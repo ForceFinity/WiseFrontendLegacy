1744dec29b60ca47136dccbb76d95bff0b0a3aba
Kirill Ibragimov <hexchap@gmail.com>
2024-11-30 23:44:24 +0200

    waking up button in bus stop popup

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -2,15 +2,17 @@ import Mapcomponent from '@/components/ui/mapcomponent.tsx';
 import PopupBusStop from '@/components/ui/popupBusStop.tsx';
 import SearchBar from "@/components/ui/SearchBar.tsx";
 import Sidebar from "@/components/ui/sidebar.tsx";
+import { useState } from "react";
 
 function App() {
+    const [isAlarmOn, setIsAlarmOn] = useState(true);
 
     return (
         <div className="flex flex-col items-center h-screen">
             <SearchBar/>
             <Sidebar/>
             <Mapcomponent/>
-            <PopupBusStop/>
+            {isAlarmOn && <PopupBusStop setIsOpen={setIsAlarmOn} />}
         </div>
     );
 }
