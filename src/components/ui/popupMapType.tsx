f9c9f8aff245600a0101d2e484012a6f25d0b6c8
zxxtlz <Hrisy2015@gmail.com>
2024-12-04 19:43:27 +0200

    Updated links ( You can see the map types again)

--- src/components/ui/popupMapType.tsx
+++ src/components/ui/popupMapType.tsx
@@ -27,15 +27,15 @@ const popupMapType = () => {
                     <div className='flex flex-row justify-evenly w-full'>
 
                         <div className='flex flex-col text-center'>
-                            <img src="https://cdn.discordapp.com/attachments/1260262354105536564/1312696342304391219/image.png?ex=674d6f3c&is=674c1dbc&hm=51b4d98ac9bdc0c8adf7fec6f779980822ca1347b9c858383f6b28a34a8cb719&" className='h-16 w-16' alt="" />
+                            <img src="https://imgur.com/ei0Do2J" className='h-16 w-16' alt="" />
                             <p>По дефолт</p>
                         </div>
                         <div className='flex flex-col text-center'>
-                            <img src="https://cdn.discordapp.com/attachments/1260262354105536564/1312696465532915763/image.png?ex=674d6f5a&is=674c1dda&hm=77b334200aeabe852a51c9168985046d8563e3131e0c990b47e25a546e3e6b16&" className='h-16 w-16' alt="" />
+                            <img src="https://imgur.com/woJLsCw" className='h-16 w-16' alt="" />
                             <p>Релеф</p>
                         </div>
                         <div className='flex flex-col text-center'>
-                            <img src="https://cdn.discordapp.com/attachments/1260262354105536564/1312696495023067176/image.png?ex=674d6f61&is=674c1de1&hm=f9c5126df8792fb754bcd26dda39a05627ff8fcda7ca1f3ea0fc7f9c395cc721&" className='h-16 w-16' alt="" />
+                            <img src="https://imgur.com/bSr0xKK" className='h-16 w-16' alt="" />
                             <p>Сателит</p>
                         </div>
 
